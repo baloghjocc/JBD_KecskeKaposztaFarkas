04431e6f09034f716cb82e431f206c2981903e4c
Balogh József <bjoci93@gmail.com>
2021-04-17 02:33:36 +0200

    3. feladat
    A 3. feladat implementacioja

--- JBD_KecskeKaposztaFarkas/public_html/kecske.js
+++ JBD_KecskeKaposztaFarkas/public_html/kecske.js
@@ -1,7 +1,12 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+window.addEventListener("load",init);
 
+function init() {
+    var tomb = new Array();
 
+    for (var i = 0; i < document.getElementsByTagName("IMG").length; i++){
+        document.getElementsByTagName("IMG")[i].addEventListener("click", function(){
+        tomb.push(this.src);
+        console.log(tomb);
+    }, false);
+    }
+}
