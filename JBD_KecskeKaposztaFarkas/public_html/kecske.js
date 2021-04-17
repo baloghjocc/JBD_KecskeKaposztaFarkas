d617ab4907821c7f1b941c774aa9ee3143a06a6a
Dóriii <Driii@DESKTOP-3CRBII9>
2021-04-17 02:44:35 +0200

    5.feladat

--- JBD_KecskeKaposztaFarkas/public_html/kecske.js
+++ JBD_KecskeKaposztaFarkas/public_html/kecske.js
@@ -3,10 +3,28 @@ window.addEventListener("load",init);
 function init() {
     var tomb = new Array();
 
+    // 3. feladat
     for (var i = 0; i < document.getElementsByTagName("IMG").length; i++){
         document.getElementsByTagName("IMG")[i].addEventListener("click", function(){
         tomb.push(this.src);
         console.log(tomb);
     }, false);
     }
+
+    //5. feladat
+    for (var i = 0; i < $("img").length; i++){
+        $("img")[i].addEventListener("mouseover",keretfel);
+        $("img")[i].addEventListener("mouseout",keretle);
+    }
+
+    function $(nav) {
+    return document.querySelectorAll(nav);
+    }
+    function keretfel() {
+        this.classList.add("kiemel"); 
+    }
+    function  keretle() {
+        this.classList.remove("kiemel"); 
+    }
+
 }
